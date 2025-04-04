0fd73123b775c785b7eade2fb8cece97b52435a2
hoshimins <hoshimin.dev@gmail.com>
2025-04-04 15:35:16 +0900

    feat/コマンドライン引数でも開始できるように修正

---
+++ src/main.py
@@ -0,0 +1,71 @@
+import sys
+from utils import fetch_rss_feed
+from utils import comparison_data
+from utils import get_caption
+from utils import get_summary
+from utils.config import load_env
+from classes.database_manager import DatabaseManager
+from classes.youtube_fetcher import YoutubeFetcher
+
+
+def main(mode):
+    load_env()
+    dbManager = DatabaseManager()
+    get_data(mode, dbManager)
+
+    no_caption_record = dbManager.get_none_caption_record()
+    if not no_caption_record:
+        print("captionsがないレコードはありません")
+        return
+
+    no_caption_video_id = [record[0] for record in no_caption_record]
+
+    caption_txt = get_caption.get_caption(no_caption_video_id[0])
+
+    dbManager.save_caption_data(no_caption_video_id[0], caption_txt)
+    print("キャプションデータを保存しました")
+
+    summary_text = get_summary.get_summary(caption_txt)
+
+    dbManager.save_summary_data(no_caption_video_id[0], summary_text)
+    print("要約データを保存しました")
+
+
+def get_data(mode, dbManager):
+    """指定されたモードによって動画情報を取得する"""
+
+    if mode == "latest":
+        channel_id, channel_name = dbManager.get_channel_data()
+        RSS_URL = f"https://www.youtube.com/feeds/videos.xml?channel_id={channel_id}"
+        new_data = fetch_rss_feed.get_latest_videos(RSS_URL)
+        db_data = dbManager.get_db_data(channel_id)
+        new_data = comparison_data.compare_data(db_data, new_data)
+
+        if not new_data:
+            print("新しい動画はありません")
+            return
+
+        print(f"新しい動画は {len(new_data)} 件です")
+        dbManager.save_db_new_data(new_data, channel_id, channel_name)
+
+    elif mode == "all":
+        new_data = YoutubeFetcher.get_all_videos(channel_id)
+        dbManager.save_db_new_data(new_data, channel_id, channel_name)
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) <= 1:
+        print("引数が不足しています")
+        sys.exit(1)
+
+    if args[1] == "all":
+        mode = "all"
+    elif args[1] == "latest":
+        mode = "latest"
+    else:
+        print("無効な引数です")
+        sys.exit(1)
+
+    print(f"mode: {mode} で実行します")
+    main(mode)
